74dd5d6dc55b4869a2a484375c4acc73d7f3b9af
simplesmile-dot <zidaneagil5@gmail.com>
2026-07-16 14:52:26 +0700

    first commit

---
+++ ElseIf.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b) {
+    return (a + b) / 2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60) 
+        return "Lolos";
+    else 
+        return "Gagal";
+    }
+
+
+int main() {
+    float nilaiBI, nilaiMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris : ";
+    cin >> nilaiBI;
+    cout << "Masukkan nilai Matematika : ";
+    cin >> nilaiMT;
+
+    //rerata = (nilaiBI + nilaiMT) / 2;
+    //if (rerata >= 60) 
+    //     status = "Lulus";
+    //else
+    // status = "Gagal";
+
+    rerata = Rata_rata(nilaiBI, nilaiMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status Kelulusan = " << Status_lulus(rerata) << " ,dengan nilai rerata = " << rerata << endl;
+}
